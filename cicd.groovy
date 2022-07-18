ee20ad2b4138f9360a196d2c66dd17ff30d7ec38
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:47:05 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'tarport'), string(name: 'DESCRIPTION', 'tarport' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'tarport'), string(name: 'DESCRIPTION', 'tar saves many files together into a single tape or disk archive, and can restore individual files from the archive.' )]
         }
 }
